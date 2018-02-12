5376d23e1e019959ea6d933df0b555b027e5b616
Jag Chumber <jag@loveforweb.com>
2018-02-12 20:55:50 +0000

    Update to api url

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?APPID=b1ceec6806b78351636f519b1ea5444b&units=metric';
+const OPEN_WEATHER_MAP_URL = 'https://api.openweathermap.org/data/2.5/weather?APPID=b1ceec6806b78351636f519b1ea5444b&units=metric';
 
 module.exports = {
     getTemp(location) {
